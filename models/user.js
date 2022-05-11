66fa8218a6b7f7a8fdd20fdcc657b3f384cbca12
David Stockdale <dstock3@gmail.com>
2022-05-11 17:04:56 -0500

    Created schema for users.

---
+++ models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema(
+  {
+    name: {type: String, required: true, maxLength: 100},
+    date: {type: Date, required: true},
+    bio: {type: String, required: true, maxLength: 280},
+    profile: {type: String, required: false},
+    posts: {type: Array}
+  }
+);
+
+// Virtual for section's URL
+UserSchema
+.virtual('url')
+.get(function () {
+  return '/user/' + this.name;
+});
+
+//Export model
+module.exports = mongoose.model('User', UserSchema);
