cba85c82ee4e4d7963ec41021adb9d1e64cf86fc
David Stockdale <dstock3@gmail.com>
2022-05-12 12:16:28 -0500

    Created basic setup for section controller.

---
+++ controllers/sectionController.js
@@ -0,0 +1,41 @@
+var Section = require('../models/section');
+
+// Display list of all sections.
+exports.section_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: section list');
+};
+
+// Display detail page for a specific section.
+exports.section_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: section detail: ' + req.params.id);
+};
+
+// Display section create form on GET.
+exports.section_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: section create GET');
+};
+
+// Handle section create on POST.
+exports.section_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: section create POST');
+};
+
+// Display section delete form on GET.
+exports.section_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: section delete GET');
+};
+
+// Handle section delete on POST.
+exports.section_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: section delete POST');
+};
+
+// Display section update form on GET.
+exports.section_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: section update GET');
+};
+
+// Handle section update on POST.
+exports.section_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: section update POST');
+};
