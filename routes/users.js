ddcc8ec1b18f3f52cefd78dcc0fa3e8d744ecd14
David Stockdale <dstock3@gmail.com>
2022-05-13 11:06:18 -0500

    Created routes for users.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Require controller modules.
+var user_controller = require('../controllers/userController');
+
+// GET request for creating a user. 
+router.get('/sign-up', user_controller.user_create_get);
+
+//user request for creating user.
+router.post('/sign-up', user_controller.user_create_post);
+
+// GET request to delete user.
+router.get('/user/:id/delete', user_controller.user_delete_get);
+
+// user request to delete user.
+router.post('/user/:id/delete', user_controller.user_delete_post);
+
+// GET request to update user.
+router.get('/user/:id/update', user_controller.user_update_get);
+
+// user request to update user.
+router.post('/user/:id/update', user_controller.user_update_post);
+
+// GET request for one user.
+router.get('/user/:id', user_controller.user_detail);
+
+// GET request for list of all users.
+router.get('/users', user_controller.user_list);
 
 module.exports = router;
