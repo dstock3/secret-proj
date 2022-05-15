e6c55e75d3e8d9c454908c50cf873f141b5a9ef8
David Stockdale <dstock3@gmail.com>
2022-05-15 17:58:26 -0500

    Fleshed out the post list controller.

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,55 @@
-var Post = require('../models/posts');
+var Posts = require('../models/posts');
+var Section = require('../models/section');
+var User = require('../models/user')
+var async = require('async');
 
 // Display list of all Posts.
 exports.post_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Post list');
+    async.parallel({
+        posts: function(callback) {
+            Posts.find()
+                .exec(callback)
+        },
+        sections: function(callback) {
+            Section.find()
+                .exec(callback)
+        },
+        users: function(callback) {
+            User.find()
+                .exec(callback)
+        }
+    }, function(err, results) {
+
+        let postArray = []
+        let sectionArray = []
+        let userArray = []
+
+        for (let i = 0; i < results.posts.length; i++) {
+            let postDetails = {
+                post: results.posts[i],
+                postSectionID: JSON.stringify(results.posts[i].section),
+                postUserID: JSON.stringify(results.posts[i].author)
+            }
+            postArray.push(postDetails)
+        }
+
+        for (let y = 0; y < results.sections.length; y++) {
+            let sectionDetails = {
+                section: results.sections[y],
+                sectionID: JSON.stringify(results.sections[y]._id)
+            }
+            sectionArray.push(sectionDetails)
+        }
+
+        for (let z = 0; z < results.users.length; z++) {
+            let userDetails = {
+                user: results.users[z],
+                userID: JSON.stringify(results.users[z]._id)
+            }
+            userArray.push(userDetails)
+        }
+        res.render('post_list', { title: 'All Posts', error: err, posts: postArray, sections: sectionArray, users: userArray });
+    });
 };
 
 // Display detail page for a specific Post.
